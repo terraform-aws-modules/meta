406610f92414042d7f44f9a5c71f8a00ece12e2a
Bryant Biggs <bryantbiggs@gmail.com>
2021-12-07 09:26:06 -0500

    fix: correct linting errors and add `.releaserc.json` file to fix release process

--- modules/main/main.tf
+++ modules/main/main.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = ">= 0.12.23"
+  required_version = "~> 1.0"
 
   required_providers {
-    # There is a bug in 2.4.1 with github_branch_protection import resource - https://github.com/terraform-providers/terraform-provider-github/issues/386
     github = {
-      source  = "hashicorp/github"
-      version = "~> 2.4"
+      source  = "integrations/github"
+      version = "~> 4.0"
     }
   }
 }
--- modules/main/variables.tf
+++ modules/main/variables.tf
@@ -9,11 +9,10 @@ variable "teams" {
   type        = any
   default     = {}
 
-  //  validation {
-  //    condition     = ! contains([for v in var.teams : contains(["secret", "closed"], lookup(v, "privacy", "secret"))], false)
-  //    error_message = "The teams privacy should be only 'secret' or 'closed'. Default is 'secret'."
-  //  }
-
+  # validation {
+  #   condition     = !contains([for v in var.teams : contains(["secret", "closed"], lookup(v, "privacy", "secret"))], false)
+  #   error_message = "The teams privacy should be only 'secret' or 'closed'. Default is 'secret'."
+  # }
 }
 variable "team_memberships" {
   description = "Map of team memberships to manage"
@@ -23,8 +22,8 @@ variable "team_memberships" {
 
 variable "team_repositories" {
   description = "Map of team repositories to manage"
-  // key = team-name/repo-name
-  // value = permission (described here - https://help.github.com/articles/repository-permission-levels-for-an-organization )
+  # key = team-name/repo-name
+  # value = permission (described here - https://help.github.com/articles/repository-permission-levels-for-an-organization )
   type    = map(string)
   default = {}
 }
@@ -32,7 +31,7 @@ variable "team_repositories" {
 variable "branch_protections" {
   description = "Map of team memberships to manage"
 
-  // key = team-name/repo-name
+  # key = team-name/repo-name
   type    = any
   default = {}
 }
