0478736c8359fd48154f2a133a562936e80c2f6e
Bryant Biggs <bryantbiggs@gmail.com>
2021-12-07 08:34:54 -0500

    fix: update repository to align with current standards (#26)

--- modules/github/main.tf
+++ modules/github/main.tf
@@ -12,16 +12,16 @@ terraform {
 locals {
   clean_static_files = { for k, v in var.static_files : k => v if v != null }
 }
-//
-//resource "githubfile_file" "static" {
-//  for_each = local.clean_static_files
-//
-//  repository_owner = split("/", var.repository)[0]
-//  repository_name  = split("/", var.repository)[1]
-//  branch           = var.branch
-//  path             = lookup(each.value, "dest")
-//  contents         = file("${var.files_dir}${lookup(each.value, "src")}")
-//}
+
+# resource "githubfile_file" "static" {
+#   for_each = local.clean_static_files
+
+#   repository_owner = split("/", var.repository)[0]
+#   repository_name  = split("/", var.repository)[1]
+#   branch           = var.branch
+#   path             = lookup(each.value, "dest")
+#   contents         = file("${var.files_dir}${lookup(each.value, "src")}")
+# }
 
 resource "github_issue_label" "this" {
   for_each = var.labels
